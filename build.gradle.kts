4d471ae10ff22b7124df06918d9f958776514c08
seungjoopet <seungjoo.jeong@navercorp.com>
2024-08-02 22:48:04 +0900

    do not merge

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,9 @@ plugins {
     id("groovy")
     id("checkstyle")
     id("org.ec4j.editorconfig") version "0.1.0"
+    id("jacoco")
+//    id("org.sonarqube") version "3.5.0.2730"
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 group = "org.chupin"
@@ -26,11 +29,13 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy("jacocoTestReport")
 }
 
 checkstyle {
     configFile = file("config/checkstyle/naver-checkstyle-rules.xml")
-    configProperties = mapOf("suppressionFile" to "config/checkstyle/naver-checkstyle-suppressions.xml")
+    configProperties =
+        mapOf("suppressionFile" to "config/checkstyle/naver-checkstyle-suppressions.xml")
 }
 
 tasks.named("check") {
@@ -39,4 +44,45 @@ tasks.named("check") {
 
 editorconfig {
     excludes = listOf(".github", "gradle/**", "gradlew", "gradlew.bat", "out/**", "build/**")
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+    }
+
+    finalizedBy("jacocoTestCoverageVerification")
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            enabled = true
+
+            limit {
+                counter = ("LINE")
+                value = ("COVEREDRATIO")
+                minimum = ("0.90".toBigDecimal())
+            }
+        }
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.organization", "chupin-tech_easyvent")
+        property("sonar.projectKey", "chupin-tech")
+        property("sonar.sources", "src")
+        property("sonar.language", "java")
+        property("sonar.sourceEncoding", "UTF-8")
+//        property("sonar.test.exclusions", "jacocoExcludePatterns.join(',')")
+        property("sonar.test.inclusions", "**/*Test.groovy")
+        property("sonar.java.coveragePlugin", "jacoco")
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "build/reports/jacoco/test/jacocoTestReport.xml"
+        )
+    }
 }
---
+++ src/main/java/org/chupin/easyvent/typeresolver/FooSolver.java
@@ -0,0 +1,12 @@
+package org.chupin.easyvent.typeresolver;
+
+public class FooSolver {
+
+    public void fooSolve() {
+
+        final String foos = "hello foosolver";
+
+        System.out.println(foos);
+    }
+
+}
